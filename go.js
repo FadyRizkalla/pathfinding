5351620f4f40744b4238f2ab8d11a38ec15cbc6c
Mosh Hamedani <fadigamal57@gmail.com>
2024-08-19 20:17:38 -0400

    path finding visualizer

---
+++ go.js
@@ -0,0 +1,19 @@
+const canvas=document.getElementById("canvas1");
+const c=canvas.getContext('2d');
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+c.fillStyle = 'rgba(0, 150, 255, 0.5)';
+
+c.fillStyle = 'rgba(0, 150, 255, 0.5)';
+c.fillRect(canvas.width / 4, canvas.height / 4, canvas.width / 2, canvas.height / 2);
+ const rectangles=[];
+ const color{
+    col1:;
+    col2:;
+    col3:;
+ }
+ for(let  j=30;j<300;j+=33){
+  for(let i=150;i<300;i+=33){
+    c.fillRect(i,j,30,30)
+  }
+}
